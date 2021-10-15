cfba69bb9af4c92732546fa52947d00222120de2
aliyesineembattal <aliyeesnm@gmail.com>
2021-10-16 01:42:02 +0300

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eligendi eius facere, libero debitis vero dolor culpa iste odit unde reiciendis dolorum fugiat cumque veniam, aut accusantium, ducimus nobis! Pariatur.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id eum dolores laudantium? Tempora ratione quos possimus, ex esse eum fugiat saepe sed repellendus distinctio tempore doloremque ipsa commodi aliquam qui!</p>
+      </div>
+    </>
+  );
+};
+
+export default About;
